34bb76a728fa3609e8ccb874067dc42f6a7e1b3f
Akbarshoh <ismatov2009akb@gmail.com>
2025-02-20 02:36:45 +0500

    Demo Navbar

---
+++ src/Pages/Home/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import styles from "./style.module.scss";
+
+const LandingHome = () => {
+  return (
+    <>
+      <div className={styles.section}>
+        <div className={styles.container}>
+            
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default LandingHome;
