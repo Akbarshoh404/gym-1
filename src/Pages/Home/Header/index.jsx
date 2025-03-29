d234208c22f37a4aa812e0c3ae3a7946f8759dc5
Akbarshoh <ismatov2009akb@gmail.com>
2025-03-30 00:17:06 +0500

    responsive header

--- src/Pages/Home/Header/index.jsx
+++ src/Pages/Home/Header/index.jsx
@@ -1,15 +1,25 @@
 import React from "react";
-
 import styles from "./style.module.scss";
-
 import Navbar from "../../../Layoutes/Navbar/index";
 
 const Header = () => {
   return (
     <>
       <header className={styles.section}>
-        <Navbar />
-        <div className={styles.container}></div>
+        <div className={styles.container}>
+          <p className={styles.mainText}>WORK WITH PROFESSIONALS</p>
+          <p className={styles.subText}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
+            odio ducimus expedita quod sapiente, ullam minima. Quae, nihil
+            soluta suscipit quas numquam dicta magni adipisci vero. Unde culpa
+            corrupti est?
+          </p>
+
+          <div className={styles.buttonGroup}>
+            <button className={styles.startButton}>START NOW</button>
+            <button className={styles.learnButton}>LEARN MORE</button>
+          </div>
+        </div>
       </header>
     </>
   );
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 
 import Header from "./Header";
+import Navbar from "../../Layoutes/Navbar";
 
 const LandingHome = () => {
   return (
     <>
+      <Navbar/>
       <Header />
     </>
   );
