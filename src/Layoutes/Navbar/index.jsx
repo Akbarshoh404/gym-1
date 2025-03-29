81b1ead342601ed00058e0544e1915f61a975029
Akbarshoh <ismatov2009akb@gmail.com>
2025-03-29 23:53:43 +0500

    fixed navbar

--- src/Layoutes/Navbar/index.jsx
+++ src/Layoutes/Navbar/index.jsx
@@ -1,8 +1,13 @@
-import React from "react";
-
+import React, { useState } from "react";
 import styles from "./style.module.scss";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <>
       <nav className={styles.nav}>
@@ -11,18 +16,34 @@ const Navbar = () => {
             <p>GYMTEAM</p>
           </div>
 
-          <div className={styles.navigation}>
+          <div className={styles.navLinks}>
             <p>Home</p>
-
             <p>Shop</p>
-
             <p>Blog</p>
-
             <p>Contact</p>
           </div>
 
           <div className={styles.div}>
-            
+            <div className={styles.hamburger} onClick={toggleMenu}>
+              <div
+                className={`${styles.bar} ${isOpen ? styles.open : ""}`}
+              ></div>
+              <div
+                className={`${styles.bar} ${isOpen ? styles.open : ""}`}
+              ></div>
+              <div
+                className={`${styles.bar} ${isOpen ? styles.open : ""}`}
+              ></div>
+            </div>
+          </div>
+
+          <div
+            className={`${styles.navigation} ${isOpen ? styles.active : ""}`}
+          >
+            <p>Home</p>
+            <p>Shop</p>
+            <p>Blog</p>
+            <p>Contact</p>
           </div>
         </div>
       </nav>
